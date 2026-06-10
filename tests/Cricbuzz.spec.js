1497f78c28187cdc03bf46fb3eb6fd6e24619617
Vadde <Akshay.Vadde@thomsonreuters.com>
2026-06-10 18:32:10 +0530

    empty

--- tests/Cricbuzz.spec.js
+++ tests/Cricbuzz.spec.js
@@ -4,7 +4,7 @@ test("cricbuzz home page validation",async({page})=>{
 
     await page.goto("https://www.cricbuzz.com/");
 
-   const team = await page.getByText("KKR").first();
+   const team = page.locator("//span[contains(text(),'KKR')]").first();
    const teamScore = await team.locator('span.font-medium').textContent();
 console.log("KKR Score: ", teamScore);
 });
